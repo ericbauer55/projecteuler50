d6dcba4874ecb881c1a0b1323f13468d53cf902e
Eric Bauer <eric.bauer55@gmail.com>
2022-02-10 00:53:21 -0500

    added doc string to natural number generator

--- src/pe50/sequences.py
+++ src/pe50/sequences.py
@@ -1,4 +1,9 @@
 def generate_natural_numbers(a_max):
+    """
+    This function generates a list of positive natural numbers from 1 to @a_max
+
+    The list of natural numbers is returned
+    """
     return list(range(1,a_max+1))
 
 def generate_prime_sequence(a_max):
