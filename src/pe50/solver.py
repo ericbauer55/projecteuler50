9cfcc3fa78da90407f8458e61500d965de77eda3
Eric Bauer <eric.bauer55@gmail.com>
2022-02-10 14:06:18 -0500

    added in the partial sums helper function

--- src/pe50/solver.py
+++ src/pe50/solver.py
@@ -1,3 +1,6 @@
+from msilib import sequence
+
+
 def consec_sum_solver(a_max, A, verbose=False):
     """
     This functions finds the largest element of sequence A (and is at most @a_max) which is a partial sum of A.
@@ -53,4 +56,34 @@ def consec_sum_solver2(a_max, A, verbose=False):
     Output:
     @s_max = PLACEHOLDER
     """
-    pass
+    # 1. Find all partial sums s_k <= a_max
+    sums_dict = _get_partial_sums(a_max=a_max, A=A)
+
+
+def _get_partial_sums(a_max, A):
+    """
+    This function creates all partial sums from elements 1 to k of sequence @A.
+    The generation stops once the kth sum exceeds @a_max 
+
+    Inputs:
+    @a_max = this is the largest value in the sequence A that is allowed. 
+    @A = list of elements in the sequence of interest
+
+    Output:
+    @sums_dict = dictionary where the keys are index k and values are the partial sum s_k = sum(a1...ak) <= @a_max
+    """
+    # initialize the partial sums list 
+    k=1
+    partial_sums = [A[0]]
+    # initialize the next partial sum as the initial condition to enter the while loop
+    s_k = partial_sums[k-1]
+
+    # As long as the next partial sum is less than @a_max, append it to the list and k++
+    while (s_k <= a_max) and (k < len(A)):
+        s_k = partial_sums[k-1] + A[k]
+        if s_k <= a_max:
+            partial_sums.append(s_k)
+        k += 1
+    
+    sums_dict = {k+1:sum for i, sum in enumerate(partial_sums)}
+    return sums_dict
