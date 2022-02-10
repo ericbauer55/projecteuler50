e2b3f130a918f667eda10a46e8188bb5c0a407db
Eric Bauer <eric.bauer55@gmail.com>
2022-02-10 00:24:23 -0500

    trying to play with the file structure to get imports working

--- src/main.py
+++ src/main.py
@@ -1,21 +1,5 @@
+import sys
+sys.path.append('../')
 
-def generate_natural_numbers(a_max):
-    pass
+from pe50-solver import *
 
-def generate_prime_sequence(a_max):
-    pass
-
-def consec_sum_solver(a_max, A):
-    """
-    This functions finds the largest element of sequence A (and is at most @a_max) which is a partial sum of A.
-    In other words, the largest partial sum is the sum of consecutive elements in sequence A. 
-
-    Inputs:
-    @a_max = this is the largest value in the sequence A that is allowed. 
-    @A = list of elements in the sequence of interest
-
-    Output:
-    @s_max = the maximum partial sum that is still in @A and <= @a_max
-    """
-    # TODO: implement algorithm
-    return -1
---
+++ src/tests/test_solver.py
@@ -0,0 +1,7 @@
+import sys
+#sys.path.append('../')
+print(sys.path)
+
+import pe50.solver as s
+
+print(help(s.consec_sum_solver))
