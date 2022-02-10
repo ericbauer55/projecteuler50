6d03de2e54a3852f46b9a735ce1f9573015da26e
Eric Bauer <eric.bauer55@gmail.com>
2022-02-10 14:14:16 -0500

    adding tests for the partial sum generator

--- src/test_solver.py
+++ src/test_solver.py
@@ -18,7 +18,7 @@ def test_generate_prime_sequence():
     assert A_prime == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 
 ##############################################################################################
-# Solver Tests
+# Solver (original) Algorithm Tests
 ##############################################################################################
 
 def test_consec_sum_solver():
@@ -28,19 +28,33 @@ def test_consec_sum_solver():
 
     assert consec_sum_solver(a_max=a_max, A=A_natural) == 91 # see README for explaination
 
+##############################################################################################
+# Solver (new) Algorithm Tests
+##############################################################################################
+
+def test__get_partial_sums():
+    # validate that the function returns the correct dictionary of partial sums
+    # use natural numbers to confirm their triangular number sums
+    a_max = 50
+    A_natural = generate_natural_numbers(a_max=a_max)
+
+    triangular_sums_dict = {1:1, 2:3, 3:6, 4:10, 5:15, 6:21, 7:28, 8:36, 9:45}
+
+    assert _get_partial_sums(a_max=a_max, A=A_natural) == triangular_sums_dict
+
 def test_pe_case1():
     # Test the Project Euler specific cases
     # Case 1:
     a_max = 100
     A_prime = generate_prime_sequence(a_max=a_max)
-    assert consec_sum_solver(a_max=a_max, A=A_prime, verbose=True) == 41
+    assert consec_sum_solver2(a_max=a_max, A=A_prime, verbose=True) == 41
 
 def test_pe_case2():
     # Test the Project Euler specific cases
     # Case 2:
     a_max = 1000
     A_prime = generate_prime_sequence(a_max=a_max)
-    assert consec_sum_solver(a_max=a_max, A=A_prime, verbose=True) == 953
+    assert consec_sum_solver2(a_max=a_max, A=A_prime, verbose=True) == 953
 
 
 
